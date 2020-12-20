7b64e17249ed1d6d2098b090409c74237ebc4159
mantasrazg <mantasrazgaitis@gmail.com>
2020-12-20 23:37:57 +0200

    Created globalStyle, new font set

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
 import "normalize.css";
+import GlobalStyle from "./globalStyle";
 import { ThemeProvider } from "styled-components";
 import theme from "./theme";
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyle />
       <Routes />
     </ThemeProvider>
   </React.StrictMode>,
