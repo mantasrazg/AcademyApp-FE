281cc08b5aebb6cd9d2f60c02adb9763e36b6506
mantasrazg <mantasrazgaitis@gmail.com>
2020-12-21 17:37:49 +0200

    Button style updated

--- src/components/Button/Button.style.js
+++ src/components/Button/Button.style.js
@@ -1,18 +1,18 @@
 import styled from "styled-components";
 
 export const Button = styled.button`
-  background: ${(props) => (props.color === "primary" ? "#eee" : "#555")};
-  color: ${(props) => (props.color === "primary" ? "#000" : "#fff")};
+  background: ${(props) => props.theme.secondary.background};
+  color: ${(props) => props.theme.secondary.color};
   box-sizing: border-box;
   outline: none;
   border: none;
-  border-radius: 5px;
-  padding: 0.5em 2em;
+  border-radius: 2px;
+  padding: 0.8rem 2rem;
   cursor: pointer;
   &:hover {
-    background: ${(props) => (props.color === "primary" ? "#999" : "#999")};
+    background: ${(props) => props.theme.secondary.hoverBackground};
   }
   &:focus {
-    background: ${(props) => (props.color === "primary" ? "#eee" : "#999")};
+    background: ${(props) => props.theme.secondary.hoverBackground};
   }
 `;
