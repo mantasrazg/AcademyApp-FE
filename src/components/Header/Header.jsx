28f385a7c857a2256d1851f143378403547188cc
mantasrazg <mantasrazgaitis@gmail.com>
2020-12-21 14:09:57 +0200

    Header update (loggedIn status)

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,15 +3,24 @@ import * as S from "./Header.style";
 import { Link } from "react-router-dom";
 import logoImg from "../../assets/Logo.png";
 
-function Header() {
+function Header({ loggedIn, logout }) {
   return (
     <S.Header>
       <Link to="/">
         <S.Logo src={logoImg} alt="Logo" />
       </Link>
       <S.Actions>
-        <S.StyledLink to="/">Home</S.StyledLink>
-        <S.StyledLink to="/about">About</S.StyledLink>
+        {!loggedIn ? (
+          <S.StyledLink to="/login">Login</S.StyledLink>
+        ) : (
+          <>
+            <S.StyledLink to="/">Home</S.StyledLink>
+            <S.StyledLink to="/students">Students</S.StyledLink>
+            <S.StyledLink to="/lecturers">Lecturers</S.StyledLink>
+            <S.StyledLink to="/courses">Courses</S.StyledLink>
+            <S.StyledLink onClick={logout}>Logout</S.StyledLink>
+          </>
+        )}
       </S.Actions>
     </S.Header>
   );
