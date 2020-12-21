57fd7c38e67de3935b02d3ecb5dc949e0f7f0179
mantasrazg <mantasrazgaitis@gmail.com>
2020-12-21 04:25:03 +0200

    Created AuthContext & imported to index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
+import { AuthProvider } from "./context/AuthContextƒ";
 import "normalize.css";
 import GlobalStyle from "./globalStyle";
 import { ThemeProvider } from "styled-components";
@@ -10,7 +11,9 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
